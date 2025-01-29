73faf8f8064d1cad60443e46279d57a982fd4614
fredini-it <142542388+fredini-it@users.noreply.github.com>
2025-01-29 16:20:34 -0300

    Update GoogleStreetViewDepthLoader_B1.js

--- src/GoogleStreetViewDepthLoader_B1.js
+++ src/GoogleStreetViewDepthLoader_B1.js
@@ -42,12 +42,12 @@ class GoogleStreetViewDepthLoader {
     return depthMap;
   }
 
-  parseHeader(depthMap) {
+ parseHeader(depthMap) {
     return {
       headerSize: depthMap.getUint8(0),
       numberOfPlanes: depthMap.getUint16(1, true),
-      width: depthMap.getUint16(3, true),
-      height: depthMap.getUint16(5, true),
+      width: depthMap.getUint16(3, true) * 2, // Aumentar a largura em 2x
+      height: depthMap.getUint16(5, true) * 2, // Aumentar a altura em 2x
       offset: depthMap.getUint16(7, true),
     };
   }
@@ -90,19 +90,22 @@ class GoogleStreetViewDepthLoader {
     const cos_phi = new Float32Array(w);
 
     for (let y = 0; y < h; ++y) {
-      const theta = ((h - y - 1.0) / (h - 1.0)) * Math.PI;
+      const theta = ((h - y - 0.5) / h) * Math.PI;
       sin_theta[y] = Math.sin(theta);
       cos_theta[y] = Math.cos(theta);
     }
     for (let x = 0; x < w; ++x) {
-      const phi = ((w - x - 1.0) / (w - 1.0)) * 2 * Math.PI + Math.PI / 2;
+      const phi = ((w - x - 0.5) / w) * 2 * Math.PI + Math.PI / 2;
       sin_phi[x] = Math.sin(phi);
       cos_phi[x] = Math.cos(phi);
     }
 
     for (let y = 0; y < h; ++y) {
       for (let x = 0; x < w; ++x) {
-        const planeIdx = indices[y * w + x];
+        // Mapeando os índices da resolução original para a nova resolução
+        const origX = Math.floor(x / 2);
+        const origY = Math.floor(y / 2);
+        const planeIdx = indices[origY * (w / 2) + origX];
 
         v[0] = sin_theta[y] * cos_phi[x];
         v[1] = sin_theta[y] * sin_phi[x];
@@ -117,7 +120,7 @@ class GoogleStreetViewDepthLoader {
           );
           depthMap[y * w + (w - x - 1)] = t;
         } else {
-          depthMap[y * w + (w - x - 1)] = 1.0;
+          depthMap[y * w + (w - x - 1)] = 9999999999999999999;
         }
       }
     }
@@ -144,14 +147,13 @@ class GoogleStreetViewDepthLoader {
 
   createEmptyDepthMap() {
     const depthMap = {
-      width: 1024,
-      height: 512,
+      width: 1024, // Dobrado
+      height: 512, // Dobrado
       depthMap: new Float32Array(1024 * 512),
     };
     for (let i = 0; i < 1024 * 512; ++i)
       depthMap.depthMap[i] = Number.MAX_SAFE_INTEGER;
     return depthMap;
   }
-}
 
 export { GoogleStreetViewDepthLoader };
