8ba865a8781ce82a2a47cff442093a3a5e6c849d
fredini-it <lucasfredini@gmail.com>
2025-04-28 14:55:57 -0300

    0

--- src/PanomNom.js
+++ src/PanomNom.js
@@ -1,4 +1,5 @@
 import { GoogleStreetViewLoader } from "./GoogleStreetViewLoader.js";
+import { GoogleStreetViewDepthLoader } from "./GoogleStreetViewDepthLoader.js";
 import { getIdByLocation, getIdFromURL } from "./utils.js";
 
-export { GoogleStreetViewLoader, getIdByLocation, getIdFromURL };
+export { GoogleStreetViewLoader, GoogleStreetViewDepthLoader, getIdByLocation, getIdFromURL };
